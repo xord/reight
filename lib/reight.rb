c7b3b9e1856d695a3980126eba93775bf7032178
xordog <xordog@gmail.com>
2024-11-08 01:08:42 +0900

    Add helpers.rb

--- lib/reight.rb
+++ lib/reight.rb
@@ -2,6 +2,7 @@ require 'rubysketch'
 
 require 'reight/project'
 require 'reight/history'
+require 'reight/helpers'
 
 require 'reight/reight'
 require 'reight/app'
--- lib/reight/app/sprite.rb
+++ lib/reight/app/sprite.rb
@@ -1,51 +1,6 @@
 using RubySketch
 
 
-module Activatable
-
-  def initialize()
-    super()
-    @active, @activateds = false, []
-  end
-
-  def active=(active)
-    active  = !!active
-    return if active == @active
-    @active = active
-    activated!
-  end
-
-  def active? = @active
-
-  def activated(&block)
-    @activateds.push block if block
-  end
-
-  def activated!()
-    @activateds.each {_1.call active}
-  end
-
-end# Activatable
-
-
-module Clickable
-
-  def initialize()
-    super()
-    @clickeds = []
-  end
-
-  def clicked(&block)
-    @clickeds.push block if block
-  end
-
-  def clicked!()
-    @clickeds.each {_1.call self}
-  end
-
-end# Clickable
-
-
 class Button
 
   include Activatable
