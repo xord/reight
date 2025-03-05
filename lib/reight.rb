03a91514b614e0cee77e18b117cd076c8e190321
xordog <xordog@gmail.com>
2025-03-05 13:16:40 +0900

    Define Sprite and Sound classes in the main context

--- lib/reight.rb
+++ lib/reight.rb
@@ -4,11 +4,13 @@ require 'reight/all'
 begin
   w, c = Reight::WINDOW__, Reight::CONTEXT__
 
-  excludes = %i[Sound Sprite]
+  reight_classes = %i[Sprite Sound]
   c.class.constants
     .reject {_1 =~ /__$/}
-    .reject {excludes.include? _1}
+    .reject {reight_classes.include? _1}
     .each   {self.class.const_set _1, c.class.const_get(_1)}
+  reight_classes
+    .each {self.class.const_set _1, Reight.const_get(_1)}
 
   w.__send__ :begin_draw
   at_exit do
